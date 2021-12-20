d008d56b1557547328cbe54ed456ac9056bdb3ad
Pamela Maura Casullo <pamela.casullo@gmail.com>
2021-12-20 17:50:58 +0100

    Activity Creations

---
+++ App Android/app/src/main/java/it/rizzoli/RED/Login.java
@@ -0,0 +1,65 @@
+package it.rizzoli.RED;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    //istanziamo un menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu,menu);
+
+        return true;
+    }
+    //eseguiamo operazioni nel menu
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        int id_item = item.getItemId();
+
+        switch (id_item) {
+            case R.id.HOMEPAGE:
+                Intent intentHome = new Intent(this,Homepage.class);
+                startActivity(intentHome);
+                break;
+            case R.id.LOGIN:
+                Intent intentLogin = new Intent(this,Login.class);
+                startActivity(intentLogin);
+                break;
+            case R.id.CALENDAR:
+                Intent intentCalendar = new Intent(this,CalendarActivity.class);
+                startActivity(intentCalendar);
+                break;
+            case R.id.REGISTER:
+                Intent intentRegister = new Intent(this,RegisterActivity.class);
+                startActivity(intentRegister);
+                break;
+            case R.id.PROFILE:
+                Intent intentProfile = new Intent(this,ProfileActivity.class);
+                startActivity(intentProfile);
+                break;
+            case R.id.COMMUNICATION:
+                Intent intentCommunication = new Intent(this,CommunicationActivity.class);
+                startActivity(intentCommunication);
+                break;
+        }
+        return false;
+    }
+}
