a4ebe7f7e464a1cd160daebbf7b3ca873c4b1f6d
Pamela Maura Casullo <pamela.casullo@gmail.com>
2022-02-15 17:55:29 +0100

    Inizio creazione Controller e REST controller

---
+++ App Web/RED/src/main/java/it/red/JdbcInstitutionController.java
@@ -0,0 +1,57 @@
+package it.red;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+
+@Repository(value="MYSQL")
+public class JdbcInstitutionController implements InstitutionRepository{
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	@Override
+	public int save(Institution p) {
+		int retCode;
+		if((jdbcTemplate.update("INSERT INTO institution(name) values (?)", new Object[] {p.getName()}))==1)  
+		
+			 retCode=1;
+		
+		else retCode=-1;
+		
+		return retCode;
+	}
+		
+	@Override
+	public Institution findValueById(long id) {
+		return jdbcTemplate.queryForObject("SELECT * FROM institution where id_institution=?", BeanPropertyRowMapper.newInstance(Institution.class),id);
+
+	}
+
+	@Override
+	public List<Institution> findAll() {
+		return jdbcTemplate.query("SELECT * FROM institution", BeanPropertyRowMapper.newInstance(Institution.class));
+	}
+
+	@Override
+	public long updateValueById(Institution p) {
+		return jdbcTemplate.update("UPDATE institution SET lastName=? WHERE id_institution=?",new Object[] {p.getName(),p.getId_institution()});
+
+	}
+
+	@Override
+	public int deleteValueById(long id) {
+		return jdbcTemplate.update("DELETE FROM players WHERE id_institution=?", id);
+	}
+
+	@Override
+	public int deleteAll() {
+		return jdbcTemplate.update("DELETE ALL FROM institution");
+
+	}
+
+
+}
