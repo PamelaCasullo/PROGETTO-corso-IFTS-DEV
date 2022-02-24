f10f4abe1c2b0092a39886b1f13027ccbe23593a
Pamela Maura Casullo <pamela.casullo@gmail.com>
2022-02-24 17:31:27 +0100

    TODO CONNECTOR DEF
    ERROR LOG: CONNECTION NULL

--- App Android/app/src/main/java/it/rizzoli/RED/Database/ConnectionHelper.java
+++ App Android/app/src/main/java/it/rizzoli/RED/Database/ConnectionHelper.java
@@ -1,42 +1,36 @@
 package it.rizzoli.RED.Database;
 
 import android.os.StrictMode;
-import android.util.Log;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class ConnectionHelper {
 
-    Connection conn;
     String uname, passw, ip, port, database;
 
-    public Connection connectionClass() {
-        ip="lorenzodns.ddns.net";
-        database="RED";
-        uname="userRED";
-        passw="^XMX^Xt4dG3Kh4%e$4ErwZU#6";
-        port="3306";
 
-        StrictMode.ThreadPolicy pol =  new StrictMode.ThreadPolicy.Builder().permitAll().build();
+    public Connection connectionClass() throws ClassNotFoundException, SQLException {
+        ip = "lorenzodns.ddns.net";
+        database = "RED";
+        uname = "userRED";
+        passw = "^XMX^Xt4dG3Kh4%e$4ErwZU#6";
+        port = "3306";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        StrictMode.ThreadPolicy pol = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(pol);
 
         Connection connection = null;
         String connURL = null;
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            connURL="jdbc:mysql://"+ip+":"+port+";"+"databasename="+database+";user="+uname+";password="+passw;
-            connection= DriverManager.getConnection(connURL);
-
-
-
-        }catch (Exception exceptionx) {
-            Log.e("Error",exceptionx.getMessage());
-        }
+        Class.forName("com.mysql.jdbc.Driver");
+        connURL = "jdbc:mysql://" + ip + ":" + port + ";" + "databasename=" + database + ";user=" + uname + ";password=" + passw;
+        connection = DriverManager.getConnection(connURL);
 
         return connection;
 
-
+        //return conn;
     }
 }
