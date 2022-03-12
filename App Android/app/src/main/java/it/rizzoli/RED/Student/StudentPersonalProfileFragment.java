569835f7684566823123eb27927af799ce065334
lollopasso00 <lorenzo.passoni@outlook.com>
2022-03-12 15:12:03 +0100

    fix

--- App Android/app/src/main/java/it/rizzoli/RED/Student/StudentPersonalProfileFragment.java
+++ App Android/app/src/main/java/it/rizzoli/RED/Student/StudentPersonalProfileFragment.java
@@ -1,36 +1,22 @@
 package it.rizzoli.RED.Student;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import it.rizzoli.RED.R;
 
-public class StudentPersonalProfileFragment extends Fragment, AppCompatActivity {
+public class StudentPersonalProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_personal_profile, container, false);
 
         return view;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.fragment_personal_profile);
-
-        EditText editText = (EditText) findViewById(R.id.editTextFirstName);
-        editText.setText("This sets the text.", TextView.BufferType.EDITABLE);
     }
 }
