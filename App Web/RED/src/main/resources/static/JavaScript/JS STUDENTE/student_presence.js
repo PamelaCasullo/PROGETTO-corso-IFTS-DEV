f93db17e3922387ab09cce07840b8abf66b7195a
Dorjan Kurti <d.kurti@itsrizzoli.it>
2022-03-20 19:46:10 +0100

    Graphic fix for student_presence

--- App Web/RED/src/main/resources/static/JavaScript/JS STUDENTE/student_presence.js
+++ App Web/RED/src/main/resources/static/JavaScript/JS STUDENTE/student_presence.js
@@ -23,17 +23,11 @@ function onPageLoaded() {
 				var risultato='PRESENZA';
 				} else var risultato='ASSENZA';
                 document.getElementById("daRiempire_Presenze").innerHTML += 
-                    "<div class='container' style='margin-top: 50px;'>"+
-        "<table class='table'> <thead>"+
-         "<tr style='background-color: transparent;'>"+
-          " <th scope='col'><h4 class='titles-shadow titles-underline'>DATA</h4></th>"+
-          "<th scope='col'><h4 class='titles-shadow titles-underline'>MATERIA</h4></th>"+
-          " <th scope='col'><h4 class='titles-shadow titles-underline'>PRESENZA</h4></th></tr></thead>"+
-          " <tbody style='background-color: rgb(68, 66, 66);'><tr>"+
-          "<th scope='row'><span class='font-text' id='student_presence_date'>"+student.date+"</span></th>"+
-          "<td><span class='font-text' id='student_presence_module'>"+student.title+"</span></td>"+
-          "<td style='background-color: darkgreen;' id='style_td_presence'><span class='titles-shadow-black font-text' id='student_presence'>"+risultato+"</span>"+
-          "</td></tr></tbody></table></div>";
+                "<tr>"+
+                "<th scope='row'><span class='font-text' id='student_presence_date'>"+student.date+"</span></th>"+
+                "<td><span class='font-text' id='student_presence_module'>"+student.title+"</span></td>"+
+                "<td style='background-color: darkgreen;' id='style_td_presence'><span class='titles-shadow-black font-text' id='student_presence'>"+risultato+"</span>"+
+                "</td></tr>";
           
           
 			}
